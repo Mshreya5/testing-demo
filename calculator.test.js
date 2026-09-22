c0b65175543ad11c3a4a7af197ded241744cbe2b
Shreya <shreyapavanje@gmail.com>
2026-09-22 15:19:24 +0530

    Added valid tests

--- calculator.test.js
+++ calculator.test.js
@@ -2,9 +2,6 @@
 // it() / test() - test case
 //expect() - assertion
 const {add} = require("./calculator");
-const {sub} = require("./calculator");
-const {mul} = require("./calculator");
-const {div} = require("./calculator");
 
 describe("Add feature of the calculator",()=>{
     test("Add 2 positive numbers",()=>{
@@ -19,22 +16,6 @@ describe("Add feature of the calculator",()=>{
         expect(add(-5,-10)).toBe(-15);
     });
 
-    test("Subtract 2 negative numbers",()=>{
-        expect(sub(-5,-10)).toBe(5);
-    });
-
-     test("Subtract 2 positive numbers",()=>{
-        expect(sub(10,5)).toBe(5);
-    });
-    test("Multiply 2 positive numbers",()=>{
-        expect(mul(5,10)).toBe(50);
-    });
-     test("Multiply 2 positive numbers",()=>{
-        expect(mul(5,10)).toBe(51);
-    });
-    test("Divide 2 positive numbers",()=>{
-        expect(div(10,5)).toBe(2);
-    });
 });
 
 //yet another manifest language-  YAML
