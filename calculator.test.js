011ea883c6be2bea603e73e0710d8cca51eb7c91
Shreya <shreyapavanje@gmail.com>
2026-09-22 15:20:19 +0530

    Added valid tests

--- calculator.test.js
+++ calculator.test.js
@@ -1,21 +1,11 @@
-//describe() - Test plan
-// it() / test() - test case
-//expect() - assertion
 const {add} = require("./calculator");
 
 describe("Add feature of the calculator",()=>{
     test("Add 2 positive numbers",()=>{
         expect(add(5,10)).toBe(15);
-        //expect().toEqual();
-        //expect().toBeTruthy()
-        //expect().toBeFalsy()
-        //expect().toBeDefined()
-        //expect().toBeUndefined()
     });
     test("Add 2 negative numbers",()=>{
         expect(add(-5,-10)).toBe(-15);
     });
 
 });
-
-//yet another manifest language-  YAML
